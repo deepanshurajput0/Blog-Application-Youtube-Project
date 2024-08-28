84fc6eda5a21608643599ce884bac422bb2e0eff
deepanshu verma <deepanshuverma186@gmail.com>
2024-08-27 17:45:47 -0700

    add update controller

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -43,3 +43,44 @@ export const getAllBlogs=async(req,res)=>{
 }
 
 
+
+export const updateBlog = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, content, category } = req.body;
+    const file = req?.file;
+    const myBlog = await blogModel.findById(id);
+    if (!myBlog) {
+      return res.status(404).json({ message: 'Blog not found' });
+    }
+
+    if (title) myBlog.title = title;
+    if (content) myBlog.content = content;
+    if (category) myBlog.category = category;
+
+    if (file) {
+      const fileUri = getDataUri(file);
+      const mycloud = await cloudinary.v2.uploader.upload(fileUri.content);
+      
+      await cloudinary.v2.uploader.destroy(myBlog.image.public_id);
+      
+      myBlog.image = {
+        public_id: mycloud.public_id,
+        url: mycloud.secure_url,
+      };
+    }
+
+    await myBlog.save();
+
+    res.status(200).json({
+      message: 'Post Updated Successfully',
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Internal Server Error"
+    });
+  }
+};
+
+
--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import { authMiddleware } from '../middlewares/authMiddleware.js'
-import { createBlog, getAllBlogs } from '../controllers/blogController.js'
+import { createBlog, getAllBlogs, updateBlog } from '../controllers/blogController.js'
 import singleUpload from '../middlewares/multer.js'
 const router = express()
 
 router.post('/create',authMiddleware,singleUpload,createBlog)
 
 router.get('/allblogs', getAllBlogs)
+router.put('/update/:id', authMiddleware,singleUpload ,updateBlog)
+router.get('/delete/:id', getAllBlogs)
 
 
 export default router
