b4f4364df30fee57b01731b57c50872846cfc93d
deepanshu verma <deepanshuverma186@gmail.com>
2024-08-29 23:31:34 -0700

    define category routes

--- server/routes/categoryRoutes.js
+++ server/routes/categoryRoutes.js
@@ -1,9 +1,13 @@
 import express from 'express'
 import { authMiddleware } from '../middlewares/authMiddleware.js'
-import { createCategory } from '../controllers/categoryController.js'
+import { createCategory, deleteCategory, getAllCategories } from '../controllers/categoryController.js'
 const router = express()
 
 router.post('/create/category',authMiddleware,createCategory)
+
+router.post('/get/categories',authMiddleware,getAllCategories)
+
+router.post('/remove/category/:id',authMiddleware, deleteCategory)
 
 
 export default router
